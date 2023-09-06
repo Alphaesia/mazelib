4488475ae17bd203e9c553b89c1a23c39bbfa657
Alphaesia <creepashadowz@gmail.com>
2023-09-06 20:07:36 +1200

    Rewrite and improve some of crate::interface's sections

--- src/interface/mod.rs
+++ src/interface/mod.rs
@@ -4,7 +4,7 @@
 //!
 //! This crate splits up mazes into distinct parts, all at different levels of abstraction.
 //! It is important to understand these, as these divisions are the foundation for the library
-//! and will dictate how you call and interact with it.
+//! and will dictate how it is interacted with.
 //!
 //! There are five core components that combined make up a maze:
 //! 1. [Cells]
@@ -48,113 +48,122 @@
 //! where --- "Do I need a piece of wall here, or a path?" (And we do indeed build the maze, using
 //! renderers, which will also be discussed later).
 //!
-//! Cells are primarily defined and identified by way they connect to other cells. Each type of
-//! cell is called a *cell class*. Each maze only has cells from a single class.
+//! Cells are primarily defined by way they connect to other cells. Each type of cell is called a
+//! *cell class*. Each maze only has cells from a single class.
 //!
 //! Cells are not restricted to being square. They can be
 //! [triangles](https://www.astrolog.org/labyrnth/maze/delta.gif),
 //! [hexagons](https://www.astrolog.org/labyrnth/maze/sigma.gif),
 //! [circular sectors](https://www.astrolog.org/labyrnth/maze/theta.gif),
-//! [different shapes in the same maze](https://www.astrolog.org/labyrnth/maze/upsilon.gif),
-//! [and worse](https://www.astrolog.org/labyrnth/maze/crack.gif).
+//! [multiple shapes](https://www.astrolog.org/labyrnth/maze/upsilon.gif), or
+//! [completely irregular](https://www.astrolog.org/labyrnth/maze/crack.gif).
 //!
 //! Cell classes are generally designed to be paired up with specific (classes of) coordinate
 //! spaces.
 //!
-//! A cell has two components: a location and a value.
+//! A cell has three components: a location, an ordinal ID, and a value.
 //!
-//! ### Cell Location
+//! ### Identifying Cells
 //!
-//! A cell's location defines its absolute position in the maze and serves as the cell's ID.
-//! Each cell has a unique location. The structure (e.g. 2-tuple, 3-tuple, UUID) of a cell
-//! location is specific to each cell class. The set of all legal cell locations in the context
-//! of a maze is known as the maze's *cell space*. The size of the cell space is proportional
-//! to the size of the maze's coordinate space (since the coordinate space must be mapped to the
-//! cell space somehow).
+//! Cells can be identified in two ways: by their location, and by their ordinal ID. These two
+//! forms of identification are inextricably linked.
 //!
-//! Cell locations are represented by the [`CellLocation`][self::cell::CellLocation] trait.
-//! There is no trait for cell spaces - they are implicitly derived from a maze's
-//! cell class-coordinate space pair.
+//! A cell location is a unique absolute position in the maze. The structure (e.g. 2-tuple, 3-tuple,
+//! UUID) of a cell location is specific to each cell class. Cell locations are represented by the
+//! [`CellLocation`][self::cell::CellLocation] trait. The set of all legal cell locations in
+//! the context of a maze is known as the maze's *cell space*. The cell space is every position in
+//! a maze where it is possible to physically exist. In a way, it is the fabric of reality. There is
+//! no trait for cell spaces as they are implicitly derived from a maze's cell class and coordinate
+//! space.
 //!
-//! ### Cell Value
+//! Every cell in a maze is assigned an arbitrary ordinal integer (0, 1, 2, ...) called its
+//! *cell id*. It is represented by the [`CellID`][crate::interface::cell::CellID] struct. How
+//! locations are mapped to IDs is up to the [maze's coordinator](#cell-manager) (discussed later).
+//! However, if the cell space has an origin, that origin is always mapped to ID 0.
 //!
-//! A cell's value determines the [state][self::cell::ConnectionType] of its connections to
-//! other cells. The values a cell can take on differ between different cell classes.
+//! Here's an example of how locations *could* be mapped to IDs:
 //!
-//! Cell values are represented by the [`CellValue`][self::cell::CellValue] trait.
+//! ![The red-outlined maze, but the cells are numbered from left-to-right top-to-bottom starting from zero. Cell 14 is highlighted in green.][example-maze-cell-outlines-with-annotated-ids]
+//!
+//! ### Cell Values
+//!
+//! A cell's value determines the [movement rules][self::cell::ConnectionType] between itself and
+//! its neighbours. This may be as blunt as "this cell can be moved through" and "this cell can't",
+//! or it may be more granular. The information carried by a cell (and thus the! set of possible
+//! values) can differ between cell classes. Cell values are represented by the
+//! [`CellValue`][self::cell::CellValue] trait.
 //!
 //! ## Point
 //!
 //! A [point][self::point::Point] is a *potential junction* of a maze. This includes features
 //! such as intersections, junctions, and straight hallways. Passages are simply two connected
-//! points.
+//! points. They are represented by the [`Point`][self::point::Point] trait.
 //!
 //! Here are all the points annotated on the maze from before:
 //!
 //! ![The pixelated maze, wth a grid of blue dots overlaid.][example-maze-points]
 //!
-//! The key difference compared to cells is that points have *semantic meaning*. The relationship
-//! between points is especially important. Moving from point to point is equivalent to moving from
-//! junction to junction in a maze. They're the basis for analysing mazes (such as generating them
-//! and solving them).
+//! The key difference compared to cells is that relationship between points has *semantic meaning*.
+//! Moving from one cell to another is like moving from one patch of dirt to another. Moving from
+//! point to point is equivalent to moving from junction to junction in a maze. They're the basis
+//! for doing anything interesting with mazes (like solving them).
 //!
-//! It is easier to think about mazes in terms of points, both for humans and computers. For
-//! humans, it is easier to consider junctions, intersections, and turns than it is to single
-//! out specific cells. For computers, there can be far less cells than points, which makes
-//! it easier (and faster) to compute things. For example, our example maze has 144 cells, but
-//! only 16 points -- an order of magnitude less.
-//!
-//! You may notice that on the figure the entrances and exits are not marked as points. This is
-//! because the maze was modified after creation to add gaps in the outer boundary to indicate
-//! the entrance and exit. Since these are visual indicators for humans, they're only a part of
-//! the cell space. For the purposes of analysis, when start and end points are required, the closest
-//! points are used. In this case, they are the upper-left-most and bottom-right-most points
-//! respectively.
+//! It is easier, for both humans and computers, to think about mazes in terms of points instead of
+//! cells. For humans, it is easier to consider junctions, intersections, and turns than it is to
+//! single out specific cells. For computers, there can be far less cells than points, which makes
+//! it easier (and faster) to compute things. Our example maze has 144 cells, but only 16 points
+//! --- an order of magnitude less.
 //!
 //! There does not need to exist a one-to-one mapping between points. A point might be mapped to
-//! multiple cells, and a cell might not be mapped to a point at all. We'll come to how points
-//! influence cells later.
+//! multiple cells, and a cell might not be mapped to a point at all. Precisely how they are mapped
+//! is up to the maze's coordinator (which we'll come to soon).
 //!
-//! Points are represented by the [`Point`][self::point::Point] trait, though it's not particularly
-//! useful on its own.
+//! You may notice that on the figure the entrances and exits are not marked as points. This is an
+//! example of how the cell space captures nuances of the physical layout that aren't relevant for
+//! analysis. In this maze, if you wanted to find a path to the exit, you would just treat the point
+//! nearest to the exit as the goal itself.
 //!
 //! ## Coordinate Space
 //!
-//! The [coordinate space][self::point::CoordinateSpace] sits at the highest level of abstraction.
-//! They carry the size and shape of the maze, and know all possible semantic positions (points)
-//! within.
+//! The [coordinate space][self::point::CoordinateSpace] is the topology the maze. It defines the
+//! set of points (potential junctions) that exist in a maze, and how they (could) connect. This
+//! dictates the size and shape of the maze.
 //!
-//! It is fundamentally a [graph](https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)),
-//! with points as its vertices. The edges between vertices represent *possible* passages between
-//! points.
+//! Like how a maze's cell space is the set of all positions where it is possible to physically
+//! exist, a maze's coordinate space is the set of all positions we care about (have semantic
+//! meaning). The coordinate space lets you traverse the maze without getting bogged down in all
+//! the details of "this passage is exactly twelve cells long, but this other passage is eleven".
+//! When solving a maze, you just move from point to point until you find the "end point".
 //!
-//! When generating, solving, or analysing mazes, the coordinate space is used. It declares how
-//! points are connected to one another -- the potential passages that could exist between junctions.
+//! Specifically, coordinate spaces are
+//! [graphs](https://en.wikipedia.org/wiki/Graph_(discrete_mathematics)), with points as its
+//! vertices, potential passages between those points as its edges, and a few other bits of
+//! information (e.g. the origin). They are represented by the
+//! [`CoordinateSpace`][self::point::CoordinateSpace] trait.
 //!
 //! Here, the turquoise lines between points represent the potential ways one could move between
-//! points. Note that however not all of these potential routes end up as passages in the final maze.
+//! points. Only some of these connections are passages and can be moved through. The others are
+//! walls and impassable.
 //!
 //! ![The pixelated maze with the grid of blue dots, but now with a grid of turquoise lines. The dots lie at all intersections of the lines.][example-maze-points-connected]
 //!
-//! While the coordinate space declares the connections between points, it does not carry the state
-//! of the connection (e.g. if it's a passage or not). One way to think about it is that it describes
-//! the maze as if there were no walls. Whether a connection is a passage or wall is determined by
-//! the cells at the location.
-//!
-//! Coordinate spaces are represented by the [`CoordinateSpace`][self::point::CoordinateSpace] trait.
+//! While the coordinate space defines the connections between points, it does not carry the state
+//! of the connection (e.g. if it's a passage or not). That is determined by the cells at the
+//! location. You could imagine it as if the coordinate space describes the maze as if there were
+//! no walls.
 //!
 //! ## Buffer
 //!
 //! The [buffer][self::buffer::MazeBuffer] handles storing mazes while they are being worked on.
 //! They carry the raw data of the maze.
 //!
-//! They only store the mazes cells, indexed by their unique sequential IDs. They have no knowledge
-//! of the higher-order structure of the maze.
-//!
-//! Typically buffers will just be something like a [`Vec`][crate::implm::buffer::VecBuffer], though
-//! there's no requirement for this to be the case.
-//!
+//! They only store the mazes cells, indexed by their unique consecutive IDs. They have no knowledge
+//! of the higher-order structure of the maze. Typically buffers will just be something akin to a
+//! [`Vec`][crate::implm::buffer::VecBuffer] wrapper but other more exotic buffers are possible.
 //! Buffers are represented by the [`MazeBuffer`][self::buffer::MazeBuffer] trait.
+//!
+//! Buffers do not handle exporting mazes into more permanent forms. That is handled by
+//! [renderers](#renderer).
 //!
 //! ## Cell Manager
 //!
@@ -201,6 +210,7 @@
 //! [Templates]: #template
 #![doc = ::embed_doc_image::embed_image!("example-maze-unannotated", "src/doc/img/maze-model/example-maze-unannotated.png")]
 #![doc = ::embed_doc_image::embed_image!("example-maze-cell-outlines", "src/doc/img/maze-model/example-maze-cell-outlines.png")]
+#![doc = ::embed_doc_image::embed_image!("example-maze-cell-outlines-with-annotated-ids", "src/doc/img/maze-model/example-maze-cell-outlines-with-annotated-ids.png")]
 #![doc = ::embed_doc_image::embed_image!("example-maze-points", "src/doc/img/maze-model/example-maze-points.png")]
 #![doc = ::embed_doc_image::embed_image!("example-maze-points-connected", "src/doc/img/maze-model/example-maze-points-connected.png")]
 
